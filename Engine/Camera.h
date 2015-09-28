81472321227f92e4b4784a4a8c87b4c0f030a656
wang35666 <356661627@qq.com>
2015-09-28 21:58:43 +0800

    camera

---
+++ Engine/Camera.h
@@ -0,0 +1,26 @@
+#ifndef __CAMERA_H__
+#define __CAMERA_H__
+
+#include "common/mat4.h"
+#include "common/vec3.h"
+
+class Camera  
+{
+public:
+	Camera();
+	virtual ~Camera();
+	void Setup3DCamera();
+	void Setup2DCamera();
+	mat4* GetViewProj();
+
+private:
+	mat4 _matView;
+	mat4 _matProj;
+	vec3 _at;					
+	vec3 _pos;								
+	vec3 _look;
+	vec3 _dir;
+	mat4 _VP;
+};
+
+#endif // !defined(AFX_CAMERA_H__48B7BC27_B223_47B1_ABD4_C17EFD0A7401__INCLUDED_)
